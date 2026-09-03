c2e0ce5d649cb7c21944cd892ac4495e3efbc524
Neha Saini <145911161+neha23nov@users.noreply.github.com>
2026-09-03 21:26:00 +0530

    maximum depth

---
+++ Trees/easy/Diameter_of_Binary_Tree.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+int diameter=0;
+int height(TreeNode* root){
+    if(root==NULL){
+
+        return 0;
+    }
+    int leftH=height(root->left);
+    int rightH=height(root->right);
+    diameter=max(diameter,leftH+rightH);
+    return 1+max(leftH,rightH);
+    
+}
+    int diameterOfBinaryTree(TreeNode* root) {
+        height(root);
+
+ 
+
+        return diameter;
+        
+    }
+};
